bd5f7f81cbf9b8a36f442c135e85c3e12baa85c3
hemantdhawan99 <dhawanhd99@gmail.com>
2022-05-31 19:22:41 +0530

    commit14

--- automatedatt/settings.py
+++ automatedatt/settings.py
@@ -135,6 +135,8 @@ STATICFILES_DIRS = [
     os.path.join(BASE_DIR,'automatedatt/static')
 ]
 
+MEDIA_ROOT = os.path.join(BASE_DIR,'Dataset')
+MEDIA_URL = '/Dataset/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
